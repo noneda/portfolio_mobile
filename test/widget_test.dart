74aa42c571e560f258fa40f984bda08041a0e408
noneda <nonejda@gmail.com>
2025-02-08 10:20:36 -0500

    Folder Organize

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:portfolio_mobile/main.dart';
+import 'package:portfolio_mobile/Screens/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
